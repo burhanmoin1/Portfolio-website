882e640672dcafe2ec13c1a2a24b1692810b86db
Burhan <m.burhanmoin1@gmail.com>
2024-04-07 02:48:58 +0500

    'addedsomemenusettings'

--- src/Header.js
+++ src/Header.js
@@ -36,11 +36,39 @@ const Header = () => {
         resetText();
     };
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <div className="main-header">
-            <h1 className='HeaderLogoText' onMouseEnter={revealText} onMouseLeave={stopReveal}>B.M.</h1>
+        <div className="main-container">
+      <div className="main-header">
+        <h1 className='HeaderLogoText' onMouseEnter={revealText} onMouseLeave={stopReveal}>B.M.</h1>
+        <button className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="menu-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+      </div>
+
+      {menuOpen && (
+        <div className="menu-overlay">
+          <div className="menu">
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+              <li>Item 4</li>
+            </ul>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Header;
