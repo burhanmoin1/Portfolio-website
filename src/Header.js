64cc4a43a3f917a27ca59e6a09c59eba30e6d046
Burhan <m.burhanmoin1@gmail.com>
2024-04-07 03:43:32 +0500

    'sticking-with-the-opacity-transition-

--- src/Header.js
+++ src/Header.js
@@ -2,40 +2,6 @@ import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
-    const [revealed, setRevealed] = useState(false);
-    let interval;
-
-    const revealText = () => {
-        if (!revealed) {
-            const text = "Codecraft";
-            const header = document.querySelector('.HeaderLogoText');
-
-            header.innerHTML = 'B.M. ';
-            let index = 0;
-
-            interval = setInterval(() => {
-                header.textContent += text[index];
-                index++;
-
-                if (index === text.length) {
-                    clearInterval(interval);
-                    setRevealed(true);
-                }
-            }, 60);
-        }
-    };
-
-    const resetText = () => {
-        const header = document.querySelector('.HeaderLogoText');
-        header.textContent = 'B.M. ';
-        setRevealed(false);
-    };
-
-    const stopReveal = () => {
-        clearInterval(interval);
-        resetText();
-    };
-
     const [menuOpen, setMenuOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -45,7 +11,7 @@ const Header = () => {
     return (
         <div className="main-container">
             <div className="main-header">
-                <h1 className='HeaderLogoText' onMouseEnter={revealText} onMouseLeave={stopReveal}>B.M.</h1>
+            <h1 className='HeaderLogoText' title=' Codecraft' >B.M.</h1>
                 <button className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
                 <div className="menu-icon">
                     <span></span>
