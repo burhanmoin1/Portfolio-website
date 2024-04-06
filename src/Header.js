ef8cdc1a07eb85b5c306f414a8a77b4afbf84233
Burhan <m.burhanmoin1@gmail.com>
2024-04-07 02:01:44 +0500

    '2nd-version;

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+    const [revealed, setRevealed] = useState(false);
+    let interval;
+
+    const revealText = () => {
+        if (!revealed) {
+            const text = "Codecraft";
+            const header = document.querySelector('.HeaderLogoText');
+
+            header.innerHTML = 'B.M. ';
+            let index = 0;
+
+            interval = setInterval(() => {
+                header.textContent += text[index];
+                index++;
+
+                if (index === text.length) {
+                    clearInterval(interval);
+                    setRevealed(true);
+                }
+            }, 60);
+        }
+    };
+
+    const resetText = () => {
+        const header = document.querySelector('.HeaderLogoText');
+        header.textContent = 'B.M. ';
+        setRevealed(false);
+    };
+
+    const stopReveal = () => {
+        clearInterval(interval);
+        resetText();
+    };
+
     return (
         <div className="main-header">
-            <h1 className='HeaderLogoText' title='B.M. Codecraft' >B.M.</h1>
-            <div className='header-items'>
-                <p className='item'>Home</p>
-                <p className='item'>Portfolio</p>
-                <p className='item'>Blogs</p>
-                <p className='item'>Contact</p>
-            </div>
+            <h1 className='HeaderLogoText' onMouseEnter={revealText} onMouseLeave={stopReveal}>B.M.</h1>
         </div>
-    )
+    );
 }
 
 export default Header;
