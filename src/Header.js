283fff99b241a75640e49b26814346fb9648455b
Burhan <m.burhanmoin1@gmail.com>
2024-04-11 23:07:51 +0500

    'thursday-night-fix'

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,51 @@
 import React , { useState } from 'react';
 import './Header.css';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+    const navContainer = {
+        visible: {
+          y: 0,
+          opacity: 1,
+          transition: {
+            duration: 0.3,
+            type: 'tween', stiffness: 500
+          }
+        },
+        hidden: {
+          y: -800,
+          opacity: 0,
+          transition: {
+            duration: 0.3,
+            type: 'tween', stiffness: 500
+          }
+        }
+      };
+
     return (
         <motion.div className="main-header">
             <motion.h1 initial={{y: -100}} animate={{y:0}} transition={{type: "tween", stiffness: 500}}
                 className='HeaderLogoText' title=' Codecraft' >B.M. 
             </motion.h1>
-            <motion.button initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.8}} className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+            <motion.button initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.8}} className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
                 <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.4}} className="menu-icon">
                     <span></span>
                     <span></span>
                     <span></span>
                 </motion.div>
             </motion.button>
+            <AnimatePresence>
             {menuOpen && (
                 <motion.div
                     className="menu-overlay"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{opacity: 0}}
-                    transition={{ duration: 0.5, ease: [0.74, 0, 0.19, 1.02] }}
+                    initial="hidden"
+                    animate={menuOpen ? "visible" : "hidden"}
+                    exit="hidden"
+                    variants={navContainer}
                 >
-                    <div className="menu-items">
+                    <div menuOpen={menuOpen} className="menu-items">
                         <div>Item 1</div>
                         <div>Item 2</div>
                         <div>Item 3</div>
@@ -36,6 +53,7 @@ const Header = () => {
                     </div>
                 </motion.div>
             )}
+            </AnimatePresence>
         </motion.div>  
         );
     }
