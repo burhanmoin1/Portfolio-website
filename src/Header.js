55e96727ea445c0dcb9e864fd9690e4949d8d157
Burhan <m.burhanmoin1@gmail.com>
2024-04-11 12:13:30 +0500

    'thursday-morning-update-menu-lookingbetterthanever'

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React , { useState } from 'react';
 import './Header.css';
 import { motion } from 'framer-motion';
 
@@ -8,33 +8,35 @@ const Header = () => {
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
-
     return (
-        <motion.div className="main-container" >
-            <div className="main-header">
-            <h1 className='HeaderLogoText' title=' Codecraft' >B.M.</h1>
-                <button className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                <motion.div className="menu-icon">
+        <motion.div className="main-header">
+            <motion.h1 initial={{y: -100}} animate={{y:0}} transition={{type: "tween", stiffness: 500}}
+                className='HeaderLogoText' title=' Codecraft' >B.M. 
+            </motion.h1>
+            <motion.button initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.8}} className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.4}} className="menu-icon">
                     <span></span>
                     <span></span>
                     <span></span>
                 </motion.div>
-                </button>
-            </div>
-
+            </motion.button>
             {menuOpen && (
-                <div className="menu-overlay">
-                <div className="menu">
-                    <ul>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                    <li>Item 4</li>
-                    </ul>
-                </div>
-            </div>
+                <motion.div
+                    className="menu-overlay"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{opacity: 0}}
+                    transition={{ duration: 0.5, ease: [0.74, 0, 0.19, 1.02] }}
+                >
+                    <div className="menu-items">
+                        <div>Item 1</div>
+                        <div>Item 2</div>
+                        <div>Item 3</div>
+                        <div>Item 4</div>
+                    </div>
+                </motion.div>
             )}
-        </motion.div>
+        </motion.div>  
         );
     }
 
