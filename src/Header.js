da00a5930771df484c4b14fec1cb72a70b893b54
Burhan <m.burhanmoin1@gmail.com>
2024-04-13 03:19:58 +0500

    'sat-morning-update-nav-items-updating'

--- src/Header.js
+++ src/Header.js
@@ -8,29 +8,27 @@ const Header = () => {
     const navContainer = {
         visible: {
           y: 0,
-          opacity: 1,
           transition: {
-            duration: 0.3,
-            type: 'tween', stiffness: 500
+            duration: 0.25,
+            type: 'tween'
           }
         },
         hidden: {
-          y: -800,
-          opacity: 0,
+          y: -1200,
           transition: {
-            duration: 0.3,
-            type: 'tween', stiffness: 500
+            duration: 0.2,
+            type: 'tween'
           }
         }
       };
 
     return (
         <motion.div className="main-header">
-            <motion.h1 initial={{y: -100}} animate={{y:0}} transition={{type: "tween", stiffness: 500}}
+            <motion.h1 initial={{y: -100}} animate={{y:0}} transition={{type: "tween", stiffness: 500, delay: 0.2}}
                 className='HeaderLogoText' title=' Codecraft' >B.M. 
             </motion.h1>
             <motion.button initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.8}} className={`menu-button ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
-                <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{duration: 0.4}} className="menu-icon">
+                <motion.div className="menu-icon">
                     <span></span>
                     <span></span>
                     <span></span>
@@ -45,11 +43,12 @@ const Header = () => {
                     exit="hidden"
                     variants={navContainer}
                 >
-                    <div menuOpen={menuOpen} className="menu-items">
-                        <div>Item 1</div>
-                        <div>Item 2</div>
-                        <div>Item 3</div>
-                        <div>Item 4</div>
+                    <div className="menu-items">
+                        <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay: 0.1}}>Home</motion.div>
+                        <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay: 0.18}}>Portfolio</motion.div>
+                        <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay: 0.26}}>About</motion.div>
+                        <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay: 0.32}}>Blogs</motion.div>
+                        <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay: 0.38}}>Contact</motion.div>
                     </div>
                 </motion.div>
             )}
