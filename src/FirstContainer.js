f493fb4d4bfb686b56389fd51bf8732a199cbefb
Burhan <m.burhanmoin1@gmail.com>
2024-04-17 04:27:49 +0500

    'cursor-cirle'

--- src/FirstContainer.js
+++ src/FirstContainer.js
@@ -4,7 +4,7 @@ import "./FirstContainer.css";
 function FirstContainer () {
   return (
     <div className="first-container">
-      <h2 className='first-heading'>Hey,<br></br><span className="burhan"> I'm Burhan </span></h2>
+      <h2 className='first-heading'><span className="hey">Hey,</span><br></br><span className="burhan"> I'm Burhan </span></h2>
       <p className='first-paragraph'>— an expert full stack web developer, assisting clients with all of their web related needs.</p>
     </div>
   );
