dc99d77a0b848a8ca6b3d2c32f059d1d6a04f035
Burhan <m.burhanmoin1@gmail.com>
2024-04-17 06:32:33 +0500

    'moving-towards-an-actual-hey-waving-hand'

--- src/FirstContainer.js
+++ src/FirstContainer.js
@@ -5,7 +5,7 @@ function FirstContainer () {
   return (
     <div className="first-container">
       <h2 className='first-heading'><span className="hey">Hey,</span><br></br><span className="burhan"> I'm Burhan </span></h2>
-      <p className='first-paragraph'>— an expert full stack web developer, assisting clients with all of their web related needs.</p>
+      <p className='first-paragraph'>— an expert full stack web developer, assisting clients with all of their web development needs.</p>
     </div>
   );
 }
