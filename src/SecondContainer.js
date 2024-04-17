be9c8bd081ba2ad9aeaaadecf9f7b3c7fc739d48
Burhan <m.burhanmoin1@gmail.com>
2024-04-17 07:25:57 +0500

    'working-on-second-container'

---
+++ src/SecondContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SecondContainer.css";
+
+function SecondContainer () {
+  return (
+    <div className="second-container">
+        <div className="black-container-for-second">
+            <p className='second-paragraph'>I specialize in creating eye-catching and captivating designs. Paired with robust and comprehensive backends, my solutions effectively handle all server-side operations, including data processing, authentication, and much more. All of this is aimed at delivering a smooth and seamless user experience.</p>
+        </div>
+    </div>
+  );
+}
+
+export default SecondContainer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Header';
 import FirstContainer from './FirstContainer';
+import SecondContainer from './SecondContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <FirstContainer />
+    <SecondContainer />
   </React.StrictMode>
 );
 
