0b797dc883b00b02dc6b0134390d016e35c4e979
Burhan <m.burhanmoin1@gmail.com>
2024-04-19 00:39:44 +0500

    'arrow'

--- src/FirstContainer.js
+++ src/FirstContainer.js
@@ -2,10 +2,37 @@ import React from "react";
 import "./FirstContainer.css";
 
 function FirstContainer () {
+
+  const handleButtonClick = () => {
+    const scrollDuration = 1000; // Adjust the duration (in milliseconds) to control the speed
+    const scrollStep = 40;
+
+    let scrollAmount = 800;
+    if (window.innerWidth <= 767.98) {
+        scrollAmount = 500;
+    }
+
+    const scrollStepAmount = scrollAmount / (scrollDuration / scrollStep);
+    let currentScroll = window.scrollY;
+
+    const scroll = () => {
+        currentScroll += scrollStepAmount;
+        if (currentScroll >= window.scrollY + scrollAmount) {
+            window.scrollTo(0, window.scrollY + scrollAmount);
+        } else {
+            window.scrollTo(0, currentScroll);
+            requestAnimationFrame(scroll);
+        }
+    };
+
+    scroll();
+};
+
   return (
     <div className="first-container">
       <h2 className='first-heading'><span className="hey">Hey,</span><br></br><span className="burhan"> I'm Burhan </span></h2>
       <p className='first-paragraph'>— an expert full stack web developer, assisting clients with all of their web development needs.</p>
+      <button className="arrow-button" onClick={handleButtonClick}>&#8595;</button>
     </div>
   );
 }
